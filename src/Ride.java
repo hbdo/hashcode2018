9077bcd46d2cf8d2a145049a72acebc58a465947
Burak Doğaroğlu <hdogaroglu@ku.edu.tr>
2018-04-28 16:15:01 +0300

    Car and Ride classes added

---
+++ src/Ride.java
@@ -0,0 +1,23 @@
+import java.lang.Math;
+public class Ride {
+
+	public int id, start_x, start_y, end_x, end_y, earliest_start, latest_finish;
+	public Car car = null;
+	
+	public static int ID = 0;;
+	
+	public Ride(int start_x, int start_y, int end_x, int end_y, int earliest_start, int latest_finish) {
+		this.id = ID++;
+		this.start_x = start_x;
+		this.start_y = start_y;
+		this.end_x = end_x;
+		this.end_y = end_y;
+		this.earliest_start = earliest_start;
+		this.latest_finish = latest_finish;
+	}
+	
+	public int get_distance() {
+		return Math.abs(end_y - start_y) + Math.abs(end_x - start_x);
+	}
+	
+}
